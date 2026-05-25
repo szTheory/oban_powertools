1336dc284a71f82276a1a3f6909d35af24711357
szTheory <szTheory@users.noreply.github.com>
2026-05-25 16:48:04 +0200

    fix(ci): align test harness with proof lanes

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,23 @@
 import Config
 
-config :oban_powertools, ObanPowertools.TestRepo,
-  database: "oban_powertools_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  priv: "test/support"
+put_if_present = fn keyword, key, env_name ->
+  case System.get_env(env_name) do
+    nil -> keyword
+    value -> Keyword.put(keyword, key, value)
+  end
+end
+
+test_repo_config =
+  [
+    database: System.get_env("OBAN_POWERTOOLS_TEST_DATABASE", "oban_powertools_test"),
+    hostname: System.get_env("PGHOST", "localhost"),
+    pool: Ecto.Adapters.SQL.Sandbox,
+    priv: "test/support"
+  ]
+  |> put_if_present.(:username, "PGUSER")
+  |> put_if_present.(:password, "PGPASSWORD")
+
+config :oban_powertools, ObanPowertools.TestRepo, test_repo_config
 
 config :oban_powertools,
   ecto_repos: [ObanPowertools.TestRepo],
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,93 +1,100 @@
 ExUnit.start()
 
-Code.require_file("test/support/migrations/0_create_tables.exs")
-Code.require_file("test/support/migrations/1_phase_2_tables.exs")
-Code.require_file("test/support/migrations/2_phase_3_tables.exs")
-Code.require_file("test/support/migrations/3_phase_4_tables.exs")
-Code.require_file("test/support/migrations/4_phase_5_tables.exs")
-Code.require_file("test/support/migrations/5_phase_6_tables.exs")
+skip_db_boot? = System.get_env("OBAN_POWERTOOLS_SKIP_DB_BOOT") in ["1", "true"]
 
-{:ok, _} = ObanPowertools.TestRepo.start_link()
+unless skip_db_boot? do
+  Code.require_file("test/support/migrations/0_create_tables.exs")
+  Code.require_file("test/support/migrations/1_phase_2_tables.exs")
+  Code.require_file("test/support/migrations/2_phase_3_tables.exs")
+  Code.require_file("test/support/migrations/3_phase_4_tables.exs")
+  Code.require_file("test/support/migrations/4_phase_5_tables.exs")
+  Code.require_file("test/support/migrations/5_phase_6_tables.exs")
 
-{:ok, _, _} =
-  Ecto.Migrator.with_repo(ObanPowertools.TestRepo, fn repo ->
-    Ecto.Migrator.run(repo, Application.app_dir(:oban_powertools, "test/support/migrations"), :up,
-      all: true
-    )
+  {:ok, _} = ObanPowertools.TestRepo.start_link()
 
-    audit_tables? =
-      repo
-      |> Ecto.Adapters.SQL.query!("SELECT to_regclass('public.oban_powertools_audit_events')")
-      |> Map.fetch!(:rows)
-      |> List.first()
-      |> List.first()
+  {:ok, _, _} =
+    Ecto.Migrator.with_repo(ObanPowertools.TestRepo, fn repo ->
+      Ecto.Migrator.run(
+        repo,
+        Application.app_dir(:oban_powertools, "test/support/migrations"),
+        :up, all: true)
 
-    if is_nil(audit_tables?) do
-      Ecto.Migrator.up(repo, 0, ObanPowertools.TestRepo.Migrations.CreateTables, log: false)
-    end
+      audit_tables? =
+        repo
+        |> Ecto.Adapters.SQL.query!("SELECT to_regclass('public.oban_powertools_audit_events')")
+        |> Map.fetch!(:rows)
+        |> List.first()
+        |> List.first()
 
-    phase_2_tables? =
-      repo
-      |> Ecto.Adapters.SQL.query!("SELECT to_regclass('public.oban_powertools_limit_resources')")
-      |> Map.fetch!(:rows)
-      |> List.first()
-      |> List.first()
+      if is_nil(audit_tables?) do
+        Ecto.Migrator.up(repo, 0, ObanPowertools.TestRepo.Migrations.CreateTables, log: false)
+      end
 
-    if is_nil(phase_2_tables?) do
-      Ecto.Migrator.up(repo, 1, ObanPowertools.TestRepo.Migrations.Phase2Tables, log: false)
-    end
+      phase_2_tables? =
+        repo
+        |> Ecto.Adapters.SQL.query!(
+          "SELECT to_regclass('public.oban_powertools_limit_resources')"
+        )
+        |> Map.fetch!(:rows)
+        |> List.first()
+        |> List.first()
 
-    phase_3_tables? =
-      repo
-      |> Ecto.Adapters.SQL.query!("SELECT to_regclass('public.oban_powertools_workflows')")
-      |> Map.fetch!(:rows)
-      |> List.first()
-      |> List.first()
+      if is_nil(phase_2_tables?) do
+        Ecto.Migrator.up(repo, 1, ObanPowertools.TestRepo.Migrations.Phase2Tables, log: false)
+      end
 
-    if is_nil(phase_3_tables?) do
-      Ecto.Migrator.up(repo, 2, ObanPowertools.TestRepo.Migrations.Phase3Tables, log: false)
-    end
+      phase_3_tables? =
+        repo
+        |> Ecto.Adapters.SQL.query!("SELECT to_regclass('public.oban_powertools_workflows')")
+        |> Map.fetch!(:rows)
+        |> List.first()
+        |> List.first()
 
-    phase_4_tables? =
-      repo
-      |> Ecto.Adapters.SQL.query!("SELECT to_regclass('public.oban_powertools_heartbeats')")
-      |> Map.fetch!(:rows)
-      |> List.first()
-      |> List.first()
+      if is_nil(phase_3_tables?) do
+        Ecto.Migrator.up(repo, 2, ObanPowertools.TestRepo.Migrations.Phase3Tables, log: false)
+      end
 
-    if is_nil(phase_4_tables?) do
-      Ecto.Migrator.up(repo, 3, ObanPowertools.TestRepo.Migrations.Phase4Tables, log: false)
-    end
+      phase_4_tables? =
+        repo
+        |> Ecto.Adapters.SQL.query!("SELECT to_regclass('public.oban_powertools_heartbeats')")
+        |> Map.fetch!(:rows)
+        |> List.first()
+        |> List.first()
 
-    phase_5_tables? =
-      repo
-      |> Ecto.Adapters.SQL.query!(
-        "SELECT to_regclass('public.oban_powertools_workflow_command_attempts')"
-      )
-      |> Map.fetch!(:rows)
-      |> List.first()
-      |> List.first()
+      if is_nil(phase_4_tables?) do
+        Ecto.Migrator.up(repo, 3, ObanPowertools.TestRepo.Migrations.Phase4Tables, log: false)
+      end
 
-    if is_nil(phase_5_tables?) do
-      Ecto.Migrator.up(repo, 4, ObanPowertools.TestRepo.Migrations.Phase5Tables, log: false)
-    end
+      phase_5_tables? =
+        repo
+        |> Ecto.Adapters.SQL.query!(
+          "SELECT to_regclass('public.oban_powertools_workflow_command_attempts')"
+        )
+        |> Map.fetch!(:rows)
+        |> List.first()
+        |> List.first()
 
-    phase_6_tables? =
-      repo
-      |> Ecto.Adapters.SQL.query!(
-        "SELECT to_regclass('public.oban_powertools_workflow_recovery_sessions')"
-      )
-      |> Map.fetch!(:rows)
-      |> List.first()
-      |> List.first()
+      if is_nil(phase_5_tables?) do
+        Ecto.Migrator.up(repo, 4, ObanPowertools.TestRepo.Migrations.Phase5Tables, log: false)
+      end
 
-    if is_nil(phase_6_tables?) do
-      Ecto.Migrator.up(repo, 5, ObanPowertools.TestRepo.Migrations.Phase6Tables, log: false)
-    end
-  end)
+      phase_6_tables? =
+        repo
+        |> Ecto.Adapters.SQL.query!(
+          "SELECT to_regclass('public.oban_powertools_workflow_recovery_sessions')"
+        )
+        |> Map.fetch!(:rows)
+        |> List.first()
+        |> List.first()
 
-config = Application.get_all_env(:oban)
-{:ok, _} = Oban.start_link(config)
-{:ok, _} = ObanPowertools.TestEndpoint.start_link()
+      if is_nil(phase_6_tables?) do
+        Ecto.Migrator.up(repo, 5, ObanPowertools.TestRepo.Migrations.Phase6Tables, log: false)
+      end
+    end)
 
-Ecto.Adapters.SQL.Sandbox.mode(ObanPowertools.TestRepo, :manual)
+  config = Application.get_all_env(:oban)
+  {:ok, _} = Oban.start_link(config)
+  {:ok, _} = ObanPowertools.TestEndpoint.start_link()
+
+  Ecto.Adapters.SQL.Sandbox.mode(ObanPowertools.TestRepo, :manual)
+end
